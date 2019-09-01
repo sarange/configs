45eae8b74170cc30027920927e1785e428737fcd
sarange <sarange@protonmail.com>
2019-09-01 20:01:42 +0300

    Imporoved the uptime script

--- scripts/uptime.py
+++ scripts/uptime.py
@@ -6,7 +6,7 @@ Project: /home/sarange/.config/i3/scripts
 Created Date: Sunday, June 23rd 2019, 10:24:11 pm
 Author: sarange
 -----
-Last Modified: Wed Jul 24 2019
+Last Modified: Sun Sep 01 2019
 Modified By: sarange
 -----
 Copyright (c) 2019 sarange
@@ -14,16 +14,25 @@ Copyright (c) 2019 sarange
 Talk is cheap. Show me the code.
 '''
 from subprocess import check_output
+import re
 
 def main():
-    uptime = str(check_output('uptime')).split('up')[1].strip().split(',')[0]
-    if ':' in uptime:
-        hours = int(uptime.split(':')[0])
-        minutes = int(uptime.split(':')[1])
-        return f'{hours}h {minutes}m'
-    else:
-        hours = 0
-        minutes = int(uptime.split('min')[0].strip())
-        return f'{minutes}m'
+	uptime = str(check_output(['uptime', '-p']))[5:-3]
+	d = {
+	'minutes':'m',
+	'minute':'m',
+	'hours':'h',
+	'hour':'h',
+	'months':'M',
+	'month':'M',
+	'year':'Y',
+	'years':'Y',
+	' ':'',
+	',':' '
+	}
+	pattern = re.compile(r'\b(' + '|'.join(d.keys()) + r')\b')
+	result = pattern.sub(lambda x: d[x.group()], uptime)
+	return result 
+
 if __name__ == '__main__':
-    print(main())
+	print(main())
